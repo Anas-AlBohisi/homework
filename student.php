bf5e4a6534d4bdd6664c209413ded1df0d08227a
anas-albohisi <anas.al.bohisi@hotmail.com>
2023-06-07 23:53:54 +0200

    homework

---
+++ student.php
@@ -0,0 +1,30 @@
+<?php
+class Student {
+  private $id;
+  private $name;
+  private $email;
+  private $courses;
+
+  public function __construct($id, $name, $email, $courses = []) {
+    $this->id = $id;
+    $this->name = $name;
+    $this->email = $email;
+    $this->courses = $courses;
+  }
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function getName() {
+    return $this->name;
+  }
+
+  public function getEmail() {
+    return $this->email;
+  }
+
+  public function getCourses() {
+    return $this->courses;
+  }
+}
